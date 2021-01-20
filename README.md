e517219258cd9acc6e8a0822b0463b475b6586ff
Vitor Lima <vitorlimadev@outlook.com>
2021-01-20 15:09:18 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# Programa de Formação em Elixir (Stone)
+
+## About
+
+This application is part of a test to qualify for a Elixir and functional programming training at [Stone](https://www.stone.com.br/).
+
+The application consists on a function that takes an array of emails and an array of shopping list items, sums the value of all items, divides it by the number of emails, and returns an array of objects containing an email and an amount of equal debt for each email.
+
+- The shopping list items price is represented in brazillian cents. So every 100 = 1 BRL.
+- If there's any remainder in the division, it will be distributed to all emails equally from the first to the last.
+- As the test specified, all numbers should be integers. Adding floating points may result in incorrect result.
+- The application will throw an error if you try to use an empty list or an undefined/null value as a param.
+
+## Running the application
+
+[**NodeJS**](https://nodejs.org/en/) is required.
+
+Navigate to the root folder of the project and run:
+
+```sh
+node ./getIndividualDebt.js
+```
+
+This will run the application with the default values present in the getIndividualDebt.js file.
+
+## Changing data
+
+In the beggining of the getIndividualDebt.js file, you'll see two arrays, one to store the emails list and another to store the shopping list data. This is the default data, used when you run the application without changing anything.
+
+```js
+const emailsList = [
+  "vitorlimadev@outlook.com",
+  "another@castle.com",
+  "arrow@theknee.com",
+  "snake_eater@psychomantis.com",
+  "alucard@belmont.com",
+];
+
+const shoopingItemsList = [
+  {
+    amount: 1,
+    itemName: "Assault Cuirass",
+    price: 2000,
+  },
+  {
+    amount: 1,
+    itemName: "Tango",
+    price: 202,
+  },
+  {
+    amount: 2,
+    itemName: "Iron branch",
+    price: 143,
+  },
+];
+```
+
+The getIndividualDebt function is called with this data and returns the final awnser to the test on the shell.
+
+```js
+getIndividualDebt(emailsList, shoopingItemsList);
+```
+
+If you want to use a mocking library, or any other source of data to test, you can import the function as a module.
+
+```js
+import getIndividualDebt from "./getIndividualDebt";
+```
---
+++ getIndividualDebt.js
@@ -0,0 +1,107 @@
+/**
+ * Define your custom emails list here.
+ * @type {Array<string>}
+ */
+const emailsList = [
+  "vitorlimadev@outlook.com",
+  "another@castle.com",
+  "arrow@theknee.com",
+  "snake_eater@psychomantis.com",
+  "alucard@belmont.com",
+];
+
+/**
+ * @typedef ShoppingItem
+ * @type {object}
+ * @property {string} itemName - The item's name.
+ * @property {number} amount - The amount of the specific item.
+ * @property {number} price - Price per unit. 100 = 1 BRL.
+ */
+
+/**
+ * Define your custom shopping items list here.
+ * @type {Array<ShoppingItem>}
+ */
+const shoopingItemsList = [
+  {
+    amount: 1,
+    itemName: "Assault Cuirass",
+    price: 2000,
+  },
+  {
+    amount: 1,
+    itemName: "Tango",
+    price: 202,
+  },
+  {
+    amount: 2,
+    itemName: "Iron branch",
+    price: 143,
+  },
+];
+
+/**
+ * Returns an array of objects with an email and the value the email's owner needs to pay.
+ * @param {Array<string>} emails - Emails list.
+ * @param {Array<ShoppingItem>} items - Shopping items list.
+ */
+const getIndividualDebt = (emails, items) => {
+  // Checking empty litsts.
+  if (!emails || emails.length <= 0 || !items || items.length <= 0) {
+    throw Error("You need at least one item in each list.");
+  }
+
+  // Getting the price of all items in the shopping list.
+  const totalPrice = items.reduce(
+    (acc, curr) => acc + curr.price * curr.amount,
+    0
+  );
+
+  // Getting the value each person will pay.
+  const dividedPrice = Math.floor(totalPrice / emails.length);
+
+  // Checking for remainder.
+  let remainder = totalPrice - dividedPrice * emails.length;
+
+  // To help with readability on the shell.
+  console.log(`TOTAL: ${totalPrice}`);
+  console.log(`PEOPLE: ${emails.length}`);
+  console.log(`PRICE PER PERSON: ${dividedPrice}`);
+  console.log(`REMAINDER: ${remainder}`);
+  console.log("*************************************");
+  console.log("RESULT:");
+
+  if (remainder <= 0) {
+    const result = emails.map((email) => {
+      return {
+        email,
+        debt: dividedPrice,
+      };
+    });
+
+    return console.log(JSON.stringify(result, null, 2));
+  }
+
+  // If there's any remainder, increase the debt of each email by one.
+  const compositeResult = emails.map((email) => {
+    if (remainder > 0) {
+      remainder--;
+
+      return {
+        email,
+        debt: dividedPrice + 1,
+      };
+    }
+
+    return {
+      email,
+      debt: dividedPrice,
+    };
+  });
+
+  return console.log(JSON.stringify(compositeResult, null, 2));
+};
+
+getIndividualDebt(emailsList, shoopingItemsList);
+
+module.exports = getIndividualDebt;
